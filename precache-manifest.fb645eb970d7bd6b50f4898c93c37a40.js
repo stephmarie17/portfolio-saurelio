0f3c5a35e1f9fd05cef51d092f1acba5a9d403a1
Stephanie A <stephanie.m.aurelio@gmail.com>
2020-03-04 18:14:46 -0800

    Updates

---
+++ precache-manifest.fb645eb970d7bd6b50f4898c93c37a40.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "007df79a07fa3c607587a037a13364c0",
+    "url": "/portfolio-saurelio/index.html"
+  },
+  {
+    "revision": "04fda3c2c3a9d041f67d",
+    "url": "/portfolio-saurelio/static/css/main.5d27b7d2.chunk.css"
+  },
+  {
+    "revision": "b10c2fd0f325399cb2b8",
+    "url": "/portfolio-saurelio/static/js/2.ff609990.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/portfolio-saurelio/static/js/2.ff609990.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04fda3c2c3a9d041f67d",
+    "url": "/portfolio-saurelio/static/js/main.9b47b266.chunk.js"
+  },
+  {
+    "revision": "01da6ef38fdfd2f0962d",
+    "url": "/portfolio-saurelio/static/js/runtime-main.8743be9b.js"
+  },
+  {
+    "revision": "f34883212c163e2732466321e6aa6ca7",
+    "url": "/portfolio-saurelio/static/media/StephanieAurelioResume.f3488321.pdf"
+  },
+  {
+    "revision": "ad5b092f8d103c8557d8d735193ed436",
+    "url": "/portfolio-saurelio/static/media/headshot.ad5b092f.jpg"
+  },
+  {
+    "revision": "34d70cffd254cb2f1b789494cd6cb799",
+    "url": "/portfolio-saurelio/static/media/luke-chesser-gradient.34d70cff.jpg"
+  },
+  {
+    "revision": "68a6e1d127151338eb3f11fb729b1aa3",
+    "url": "/portfolio-saurelio/static/media/trent-erwin-desktop.68a6e1d1.jpg"
+  }
+]);
